3270c03e5acc366b38712b9b93ab314db6834a7b
Bogdan <bogdan.terebin@ya.ru>
2024-04-22 15:44:28 +0400

    1 chpter book is ready

--- sampletest/myapp/urls.py
+++ sampletest/myapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='main'),
     path('item/<int:board_id>/', views.index_page, name='index_page'),
     path('rublic/<int:rublic_id>/', views.rublic_page, name='rublic_page'),
+    path('add/', views.BbCreateView.as_view(), name='add'),
 ]
--- sampletest/myapp/views.py
+++ sampletest/myapp/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from .models import Bb, Rublic
+from .forms import BbForm
+
 
 def index(request):
     items = Bb.objects.all()
     rublics = Rublic.objects.all()
-    return render(request, 'myapp/index.html', {'bb':items, 'rublics': rublics})
+    return render(request, 'myapp/index.html', {'items':items, 'rublics': rublics})
+
 
 def index_page(request, board_id):
     res = get_object_or_404(Bb, pk=board_id)
     return render(request, 'myapp/index_page.html', {'item':res, 'rublics': Rublic.objects.all()})
+
 
 def rublic_page(request, rublic_id):
     cur_rublic = get_object_or_404(Rublic, pk=rublic_id)
     items = Bb.objects.filter(rublic=cur_rublic)
     rublics = Rublic.objects.all()
     return render(request, 'myapp/rublic_page.html', {'c_rublic':cur_rublic, 'items':items, 'rublics':rublics})
+
+
+class BbCreateView(CreateView):
+    template_name = 'myapp/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('myapp:main')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rublics'] = Rublic.objects.all()
+        return context 
